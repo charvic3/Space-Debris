a86df8205ba7b4f2f533f51a3225d434de0380f6
Dominic Curcio <dc877297@wcupa.edu>
2021-07-21 20:59:00 -0400

    this is from the next_practice folder

---
+++ web_dev_practice/next_practice/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../components/navigations'
+import Container from '../components/Container'
+
+const About = () =>{
+    return (
+        <div>
+            <Container>
+                <h1>Hello</h1>
+            </Container>
+        </div>
+        )
+}
+
+export default About
---
+++ web_dev_practice/next_practice/pages/services.js
@@ -0,0 +1,17 @@
+import Container from '../components/Container'
+import Head from 'next/head'
+
+const Services = () => {
+    return(
+    
+    <Container>
+        <Head>
+            <title>Next.js Project - Home</title>
+        </Head>
+        <h1>Services</h1>
+        <p>lorem ipsum</p>
+    </Container> 
+    )
+}
+
+export default Services
